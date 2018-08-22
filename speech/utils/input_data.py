9c7531d45ef58daa62573ce7bdf8440c2bb8996a
代祥 <daixiang@daixiang.local>
2018-08-22 17:16:36 +0800

    update

--- speech/utils/input_data.py
+++ speech/utils/input_data.py
@@ -1,38 +1,23 @@
 import os
-import scipy.io.wavfile as wav
-
-def load_wav_file(wav_path):
-    wav_files = []
-    for (dirpath, dirnames, filenames) in os.walk(wav_path):
-        for filename in filenames:
-            if filename.endswith(".wav") or filename.endswith(".WAV"):
-                filename_path = os.sep.join([dirpath, filename])
-                if os.stat(filename_path).st_size < 240000:
-                    continue
-                wav_files.append(filename_path)
-
-    return wav_files
+import numpy as np
 
 
-def load_label_file(label_file):
-    labels_dict = {}
-    with open(label_file, "r", encoding='utf-8') as f:
-        for label in f:
-            label = label.strip("\n")
-            label_id, label_text = label.split(' ', 1)
-            labels_dict[label_id] = label_text
-
-    return labels_dict
-
-
-def get_next_batches(wav_files, labels, next_idx, batch_size):
-    batches_wavs = []
-    batches_labels = []
+def get_next_batches(next_idx, files, labels, num_contexts, batch_size):
+    batches_label = []
+    batches_sample = []
     for i in range(batch_size):
-        pass
+        sample = np.loadtxt(files[next_idx])
+        sample = organization_context(sample[::2], num_contexts)
+        batches_sample.append(sample)
+        next_idx++
 
-    return batches_wavs, batches_labels, next_idx
+    return batches_labels, batches_samples, next_idx
+
+def organization_context(sample, num_contexts):
+    pass
+
+def align_samples(batches_labels, batches_samples):
+    pass
 
 if __name__ == "__main__":
-    wav_files = load_wav_file("/Users/daixiang/deep-learning/data/data_wsj/wav/train")
-    labels_dict = load_label_file("/Users/daixiang/deep-learning/data/data_wsj/doc/trans/train.word.txt")
+    pass
--- speech/utils/label_wav.py
+++ speech/utils/label_wav.py
@@ -1,7 +1,33 @@
 import os
-from collections import Counter
+import numpy as np
+import scipy.io.wavfile as wav
 
-def prepare_label_wav_list(wav_files, labels_dict):
+from collections import Counter
+from python_speech_features import mfcc
+
+def load_wav_file(wav_path):
+    wav_files = []
+    for (dirpath, dirnames, filenames) in os.walk(wav_path):
+        for filename in filenames:
+            if filename.endswith(".wav") or filename.endswith(".WAV"):
+                filename_path = os.sep.join([dirpath, filename])
+                if os.stat(filename_path).st_size < 240000:
+                    continue
+                wav_files.append(filename_path)
+
+    return wav_files
+
+def load_label_file(label_file):
+    labels_dict = {}
+    with open(label_file, "r", encoding='utf-8') as f:
+        for label in f:
+            label = label.strip("\n")
+            label_id, label_text = label.split(' ', 1)
+            labels_dict[label_id] = label_text
+
+    return labels_dict
+
+def prepare_label_list(wav_files, labels_dict):
     labels = []
     new_wav_files = []
     for wav_file in wav_files:
@@ -12,57 +38,41 @@ def prepare_label_wav_list(wav_files, labels_dict):
 
     all_words = []
     for label in labels:
-        # 字符分解
         all_words += [word for word in label]
 
     counter = Counter(all_words)
     count_pairs = sorted(counter.items(), key = lambda x: -x[1])
 
     words, _ = zip(*count_pairs)
-    words_size = len(words)
-    print("词汇表大小：", words_size)
-
     lexicon = dict(zip(words, range(len(words))))
 
-    # 将单个file的标签映射为num 返回对应list,最终all file组成嵌套list
-    labels_vector = [words2vec(label, lexicon) for label in labels]
+    return lexicon, labels, new_wav_files
 
-    return lexicon, labels_vector
+def preapre_wav_mfcc(wav_files, num_filter, path):
+    mfcc_files = []
+    for wav_file in wav_files:
+        fs, audio = wav.read(wav_file)
+        orig_inputs = mfcc(audio, samplerate = fs, numcep = num_filter)
 
-def words2vec(words, lexicon):
-    return list(map(lambda word: lexicon.get(word, len(words)), words))
+        file_name = path + os.path.basename(wav_file).split(".")[0] + ".txt"
+        np.savetxt(file_name, orig_inputs)
+        mfcc_files.append(file_name);
 
+def labels2vec(labels, lexicon):
+    to_num = lambda word: lexicon.get(word, len(lexicon))
+    return [list(map(to_num, label)) for label in labels]
 
 if __name__ == "__main__":
-    wav_files = ['/Users/daixiang/deep-learning/data/data_wsj/wav/train/B21/B21_254.wav', 
-                 '/Users/daixiang/deep-learning/data/data_wsj/wav/train/B21/B21_268.wav', 
-                 '/Users/daixiang/deep-learning/data/data_wsj/wav/train/B21/B21_250.wav', 
-                 '/Users/daixiang/deep-learning/data/data_wsj/wav/train/B21/B21_251.wav', 
-                 '/Users/daixiang/deep-learning/data/data_wsj/wav/train/B21/B21_263.wav', 
-                 '/Users/daixiang/deep-learning/data/data_wsj/wav/train/B21/B21_262.wav', 
-                 '/Users/daixiang/deep-learning/data/data_wsj/wav/train/B21/B21_261.wav', 
-                 '/Users/daixiang/deep-learning/data/data_wsj/wav/train/B21/B21_265.wav', 
-                 '/Users/daixiang/deep-learning/data/data_wsj/wav/train/B21/B21_259.wav', 
-                 '/Users/daixiang/deep-learning/data/data_wsj/wav/train/B21/B21_258.wav']
+    wav_files = load_wav_file("/Users/daixiang/deep-learning/tensorflow/data/data_wsj/wav/train")
+    labels_dict = load_label_file("/Users/daixiang/deep-learning/tensorflow/data/data_wsj/doc/trans/train.word.txt")
 
-    labels_dict = {'B21_254': '新 聘 国务院 参事 王 一平 新 聘 中央 文史 研究 馆 馆员 孙 机 程 毅 中 也 应邀 参加 了 招待会', 
-                   'B21_268': '碰上 爱玩 且 能 玩物 成 痴 玩物 成 癖 的 人 总能 令人 愉快 地 会心一笑', 
-                   'B22_250': '嫌疑犯 眼看 脱逃 无望 窜 上 屋顶 纵身 跳下 意欲 自杀 结果 摔成 骨折 被 生擒', 
-                   'B22_251': '且 夫 孥 孥 阿文 确 尚无 偷 文 如 欧阳 公 之 恶 德 而 文章 亦 较为 能 做做 者 也', 
-                   'B21_263': '中国 要求 在 别国 遗弃 化学武器 的 国家 应 按 公约 规定 尽快 彻底 销毁 所有 遗弃 化学武器', 
-                   'B21_262': '电话 是 市 公安局 派 往 黑龙江省 安达市 的 追捕 小组 打来 的 侦查员 报告 持枪 在逃犯 已 被 生擒', 
-                   'B21_261': '大 院里 也是 两派 在 骂 夜晚 也 在 斗 走资派 一天到晚 心惊肉跳 随时 准备 着 挨斗', 
-                   'B21_265': '参 战军 虽然 没有 参加 对 德 战争 但 有 十万 华工 被 派 赴 欧洲 战场 为 协约国 军队 运输 弹药 给养 和 修筑 工事', 
-                   'B21_259': '千 百年 来 逐 水草 而 居 靠 天 养 畜 的 藏族 牧民 开始 在 围栏 内 划 片 轮 牧 并 利用 农机具 种草', 
-                   'B21_258': '要 理顺 产权 关系 除了 要 理顺 政企 关系 外 首先 要 理顺 企业 资产 所有者 与 经营者 的 关系'}
+    lexicon, labels, wav_files = prepare_label_list(wav_files, labels_dict)
+    labels_vector = labels2vec(labels, lexicon)
 
-    lexicon, labels_vector = prepare_label_wav_list(wav_files, labels_dict)
-    print(wav_files[0])
-    print(labels_vector[0])
-    print(list(lexicon.keys())[17])
-    #print(words[17]) # 新
-    #/Users/daixiang/deep-learning/data/data_wsj/wav/train/B21/B21_254.wav 
-    #[17, 0, 18, 0, 2, 60, 19, 0, 4, 20, 0, 61, 0, 8, 62, 0, 17, 0, 18, 0, 9, 63, 0, 64, 65, 0, 66, 67, 0, 21, 0, 21, 22, 0, 68, 0, 23, 0, 69, 0, 70, 0, 9, 0, 10, 0, 24, 71, 0, 4, 25, 0, 26, 0, 72, 73, 27]
+    sample = 1027
+    print(wav_files[sample])
+    print(labels[sample])
+    print(labels_vector[sample])
+    print(list(lexicon.keys())[6])
 
-    label_vector = words2vec(list(labels_dict.values())[0], lexicon)
-    print(label_vector)
+    preapre_wav_mfcc(wav_files, 26, "./exp/")
